ee51c0b816b167d8ed104d5d01efef3866a06178
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-10-05 10:20:37 -0300

    Starting BDD testing practice with JBehave

---
+++ src/main/java/marianojbddtesting/steps/ThingSteps.java
@@ -0,0 +1,36 @@
+package marianojbddtesting.steps;
+
+import marianojbddtesting.model.Thing;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.junit.Assert;
+
+public class ThingSteps {
+    private final ThreadLocal<Thing> thingStash = new ThreadLocal<>();
+
+    private Thing getThing() {
+        return this.thingStash.get();
+    }
+
+    @Given("I have a Thing with an item named $name")
+    public void givenIHaveAThingWithAnItemNamed(String name) {
+        Thing thing = new Thing(name);
+        thingStash.set(thing);
+    }
+
+    @When("I add an item named $name")
+    public void whenIAddAnItemNamed(String name) {
+        getThing().addThing(name);
+    }
+
+    @Then("My thing has $count items")
+    public void thenThingHasName(int count) {
+        Assert.assertEquals(getThing().getThingCount(), count);
+    }
+
+    @Then("My thing has an item named $name")
+    public void thenThingHasName(String name) {
+        Assert.assertTrue(getThing().hasThing(name));
+    }
+}
